f4b1ab558681261abd732a19224eb5c0bbf7f7e8
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-24 21:36:36 -0300

    Add package associations and validations

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,2 +1,11 @@
 class Package < ApplicationRecord
+  has_many :bookings
+  has_many :users, through: :bookings
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :description, presence: true, length: { maximum: 300 }
+  validates :destination, presence: true, length: { maximum: 30 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :flight, inclusion: [true, false]
+  validates :hotel, presence: true, length: { maximum: 30 }
 end
