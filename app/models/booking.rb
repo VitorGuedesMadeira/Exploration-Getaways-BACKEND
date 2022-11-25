117973a24a8877bc373b5547bad204b0592e98e6
Nyame-Wolf <mumenyam@gmail.com>
2022-11-25 18:14:09 +0300

    add validations to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :package
+
+  validates :start_date, comparison: { greater_than: :end_date }
+  validates :start_date, presence: true, numericality: { greater_than: Date.today.to_time.to_i }
+  validates :end_date, presence: true, numericality: { greater_than: Date.today.to_time.to_i }
 end
