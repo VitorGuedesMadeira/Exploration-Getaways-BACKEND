761d3d21b8bb86e970c4f8e9e6f0e22878c44f9b
Nyame-Wolf <mumenyam@gmail.com>
2022-11-30 23:09:44 +0300

    Authenticate booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     render json: currentuser.bookings.all
