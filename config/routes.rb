5d903dfe06f79adfe38082c68e2fa4c5fcde2293
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-12-05 12:03:02 -0300

    Add current user index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get '/current_user', to: 'current_user#index'
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -10,9 +9,9 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   
+  resources :users
   resources :bookings
   resources :packages
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
    root "packages#index"
