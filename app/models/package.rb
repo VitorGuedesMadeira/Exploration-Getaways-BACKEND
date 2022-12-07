f9efe1f6ac0b360c813decf8131d1a8bb052d7a8
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-12-07 12:04:08 -0300

    Change description validations and seeds

--- app/models/package.rb
+++ app/models/package.rb
@@ -3,7 +3,7 @@ class Package < ApplicationRecord
   has_many :users, through: :bookings
 
   validates :title, presence: true, length: { maximum: 50 }
-  validates :description, presence: true, length: { maximum: 300 }
+  validates :description, presence: true, length: { maximum: 600 }
   validates :destination, presence: true, length: { maximum: 30 }
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :flight, inclusion: [true, false]
